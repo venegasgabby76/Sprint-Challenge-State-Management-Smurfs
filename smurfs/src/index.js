e337c63fd0b336146b971578f5754dec1179b6a6
venegasgabby76 <gabbyvenegas76@yahoo.com>
2020-06-26 16:36:45 -0700

    added my dependancies , answered the read me and got a dummy reducer going to hook up my index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';  
+import {smurfReducer} from "./reducer/reducer"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore ( smurfReducer , applyMiddleware(thunk))
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
---
+++ smurfs/src/reducer/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    smurfs: [],
+    isFetching: false,
+    error: ""
+}
+
+export const smurfReducer = (state = initialState , action ) => {
+    switch(action.type) {
+        default: 
+        return state;
+    }
+}
